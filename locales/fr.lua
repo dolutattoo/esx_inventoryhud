001e68876ec4dd211f461ea258c79a651eaee4d6
Dolu Tattoo <47056777+dolutattoo@users.noreply.github.com>
2019-05-23 12:13:54 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -3,9 +3,9 @@ Locales["fr"] = {
 	["black_money"] = "Argent sale",
 	["player_nearby"] = "Le joueur sélectionné n'est plus près de vous.",
 	["players_nearby"] = "Il n'y a personne près de vous.",
-	["openinv_help"] = "Opens inventory of another player",
-	["openinv_id"] = "Player ID",
-	["no_permissions"] = "You don't have permissions to do that!",
-	["no_player"] = "Didn't find player with given ID!",
-	["player_inventory"] = "Player inventory"
+	["openinv_help"] = "Ouvrir l'inventaire d'un autre joueur",
+	["openinv_id"] = "ID joueur",
+	["no_permissions"] = "Permissions insuffisantes",
+	["no_player"] = "ID introuvable",
+	["player_inventory"] = "Inventaire joueur"
 }
